96308a35778d43f83ab0bbc5f5cfe346bf1c9e8e
horaanlika-droid <horaanlika@gmail.com>
2026-07-27 23:51:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
- 
+import asyncio
+
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+
+from config import BOT_TOKEN
+
+dp = Dispatcher()
+
+bot = Bot(
+    token=BOT_TOKEN,
+    default=DefaultBotProperties(
+        parse_mode=ParseMode.HTML
+    )
+)
+
+
+async def main():
+    print("🍜 SANUKI BOT запущен!")
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main()) 
