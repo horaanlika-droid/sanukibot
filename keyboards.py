c4fd9aad6900e1dd082cab23d31537726f803450
horaanlika-droid <horaanlika@gmail.com>
2026-07-28 00:10:43 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,1 +1,11 @@
- 
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+def main_menu_keyboard():
+    buttons = [
+        [InlineKeyboardButton(text="🍽 В кафе", callback_data="dine_in")],
+        [InlineKeyboardButton(text="🥡 С собой", callback_data="takeaway")],
+        [InlineKeyboardButton(text="🛵 Доставка", callback_data="delivery")],
+        [InlineKeyboardButton(text="📋 История заказов", callback_data="history")],
+        [InlineKeyboardButton(text="ℹ️ О SANUKI", callback_data="about")],
+    ]
+    return InlineKeyboardMarkup(inline_keyboard=buttons)
