aa123e1e6f8c53c72ae89c86f8f74d24b5fca5d5
horaanlika-droid <horaanlika@gmail.com>
2026-07-27 23:51:12 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,13 @@
- 
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+ADMIN_IDS = [
+    int(x)
+    for x in os.getenv("ADMIN_IDS", "").split(",")
+    if x
+] 
